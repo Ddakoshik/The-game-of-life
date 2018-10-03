60ad07a582ee7bec4ae73ebc5829f1e1a2626f3f
Andrii Ishchenko <aishchenko@deloitte.com>
2018-10-03 17:53:40 +0300

    v0.1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
-var cols = 10;
-var rows = 10;
-
-var colors = [];
+let cols;
+let rows;
+let resolution = 20;
+let grid;
 
 
 function make2Darrey(cols, rows) {
@@ -12,27 +12,77 @@ function make2Darrey(cols, rows) {
     return arr;
 }
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(200, 200);
+    cols = width / resolution;
+    rows = height / resolution;
+    grid = make2Darrey(cols, rows);
     for (var i = 0; i < cols; i++) {
-        colors[i] = [];
-        for(var j = 0; j < rows; j++) {
-            colors[i][j] = random(255);
-
+        for (var j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2));
         }
     }
+
 }
 
 function draw() {
-    background(255);
+    background(0);
+
+
 
     for (var i = 0; i < cols; i++) {
-        for(var j = 0; j < rows; j++) {
-            var x = i * 30;
-            var y = j * 30;
-            fill(colors[i][j]);
+        for (var j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = j * resolution;
+            if(grid[i][j] == 1){
+                fill(255);
+            } else {
+                fill(0);
+            }
             stroke(0);
-            rect(x, y, 30, 30);
+            rect(x, y, resolution-1, resolution-1);
+        }
+    }
+
+    let next = make2Darrey(cols, rows);
+
+    // Compute next based on grid
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+
+            if (i == 0 || i == cols - 1 || j == 0 || j == rows - 1) {
+                next[i][j] = grid[i][j]
+            }
+        // Count live neibhhors!
+
+
+        let sum = 0;
+        let neibhhors = count(grid, i ,j);
+        let state = grid[i][j];
+
+        // sum += grid[i - 1] [j - 1]
+        // sum += grid[i] [j - 1]
+        // sum += grid[i + 1] [j - 1]
+        // sum += grid[i - 1] [j]
+        // sum += grid[i + 1] [j]
+        // sum += grid[i - 1] [j + 1]
+        // sum += grid[i] [j + 1]
+        // sum += grid[i + 1] [j + 1]
 
         }
     }
+
+    grid = next;
+}
+
+
+function countNeighbors(grid, x, y){
+    let sum = 0;
+    for ( let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            if(!grid[x][y]){
+                sum += grid[i][j];
+            }
+        }
+    }
+    return sum;
 }
